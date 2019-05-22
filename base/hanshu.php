ba26ad4bd92dce2073d5e1d3b0c5db1b36455e1e
springyang <384317693@qq.com>
2019-05-22 15:18:01 +0800

    超全局变量

--- base/hanshu.php
+++ base/hanshu.php
@@ -1,5 +1,11 @@
 <?php
 
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
 function  getName($name='hcy2017'){
 
     echo __FUNCTION__;
@@ -7,4 +13,9 @@ function  getName($name='hcy2017'){
 }
 
 
-echo getName('hcy2020');
+$a="hcy2011";
+
+//echo getName('hcy2020');
+
+
+echo $GLOBALS['a'];
