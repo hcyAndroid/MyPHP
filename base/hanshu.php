f2efe460a1fac207999bd5a299f0d46bc8a452f1
springyang <384317693@qq.com>
2019-05-22 16:36:50 +0800

    函数

--- base/hanshu.php
+++ base/hanshu.php
@@ -1,21 +1,71 @@
 <?php
 
 
-
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 
-function  getName($name='hcy2017'){
+//
+//$a="hcy2011";
+//
+//
+//function  getName($name='hcy2017'){
+//
+//    //$GLOBALS['a']=$name;
+//
+//    //return $name;
+//}
+//
+//
+////echo getName('hcy2020');
+//
+//
+////echo $a;
+///
+///
 
-    echo __FUNCTION__;
-    //return $name;
+
+//$a="555555";
+//
+//function  test(){
+//    global  $a;
+//    echo  $a;
+//}
+//
+//
+//test();
+
+
+//function test()
+//{
+//    echo "hellow";
+//}
+//
+//$a = 'test';
+//$a();
+
+
+//$a=function(){
+//    echo "hellow111";
+//};
+//
+//$a();
+//
+//(function(){
+//    echo "hellow2222";
+//})();
+
+
+function  func1(){
+    $num=20001;
+    $func2=function () use ($num){
+        echo $num;
+    };
+
+    $func2();
 }
 
 
-$a="hcy2011";
-
-//echo getName('hcy2020');
+func1();
 
 
-echo $GLOBALS['a'];
