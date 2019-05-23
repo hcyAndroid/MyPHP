eeaae837dc47c85d54e6d30ef3f5c22a7affdd51
springyang <384317693@qq.com>
2019-05-23 11:57:41 +0800

    字符串常用方法

--- base/zifuchuang.php
+++ base/zifuchuang.php
@@ -2,10 +2,7 @@
 
 
 //nowdoc
-$a=<<<'eod'
-myhcy2019dsadascascascsacdas
-asdsadasdas
-eod;
+/*
 
 
 
@@ -15,6 +12,33 @@ eod;
 
 echo $a."<br>";
 
-echo $b."<br>";
+echo $b."<br>";*/
+
+
+$a=<<<eod
+myhcy2019dsadascascascsacdas
+asdsadasdas
+eod;
+
+
+$a="hcy2019";
+
+$tag="哈哈哈哈";
+
+//$tag = <<<eod
+//    <script>
+//        alert('$a')
+//    </script>
+//eod;
+//
+//echo $tag;
+
+
+strlen($a);
+echo strlen($a),"<br>",strlen($tag),"<br>";
+echo mb_strlen($a),"<br>",mb_strlen($tag,"utf-8");
+
+
+
 
 
