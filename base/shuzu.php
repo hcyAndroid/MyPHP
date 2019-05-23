77ab41cf21ab6d502d8662e4241b7018d07d0d03
springyang <384317693@qq.com>
2019-05-23 15:58:16 +0800

    数组的定义

---
+++ base/shuzu.php
@@ -0,0 +1,14 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
+
+/*
+$arr[1]='1';
+$arr[2]='2';
+
+var_dump($arr[0]);*/
+
+
+$arr=array('hcy','love','cmj',2019);
+var_dump($arr);
