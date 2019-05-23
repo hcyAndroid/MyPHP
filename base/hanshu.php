4a58dd750d0e1460e82807cc484e7c4b9e9fef62
springyang <384317693@qq.com>
2019-05-23 11:02:09 +0800

    字符串

--- base/hanshu.php
+++ base/hanshu.php
@@ -1,8 +1,11 @@
 <?php
 
+//让浏览器以utf-8的格式解析
+header('Content-type:text/html;charset=utf-8');
 
-error_reporting(E_ALL);
 ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
+//error_reporting(E_ALL);
 
 
 //
@@ -68,12 +71,67 @@ ini_set('display_errors', '1');
 //
 //func1();
 
-function f(){
-    echo "hekk";
+
+//function f()
+//{
+//    echo "hekk";
+//}
+//
+//function_exists('f');
+//
+//
+//echo function_exists('f');
+
+
+/*E_USER_NOTICE
+E_USER_WARNING
+E_USER_ERROR
+E_USER_DEPRECATED*/
+
+
+//trigger_error("空指针异常", E_USER_DEPRECATED);
+
+
+/**
+ * @param $errNum
+ * @param $errMsg
+ * @param $errFile
+ * @param $errLine
+ * @param $errContext
+ */
+
+function error_msg($errNum, $errMsg, $errFile, $errLine, $errContext)
+{
+    //查看当前系统可以看到的错误
+    if (!(error_reporting() & $errNum)) {
+        return false;
+    }
+    switch ($errNum) {
+        case E_ERROR:
+        case E_USER_ERROR:
+            echo "在" . $errFile . "文件里的第" . $errLine . "行发生fatal Error,错误信息为:" . $errMsg;
+            break;
+        case E_WARNING:
+        case E_USER_WARNING:
+            echo "在" . $errFile . "文件里的第" . $errLine . "行发生Warning,错误信息为:" . $errMsg;
+            break;
+
+        case E_NOTICE:
+        case E_USER_NOTICE:
+            echo "在" . $errFile . "文件里的第" . $errLine . "行发生E_NOTICE,错误信息为:" . $errMsg;
+            break;
+
+    }
+    return true;
 }
-function_exists('f');
 
 
-echo function_exists('f');
+
+
+//修改错误机制
+
+set_error_handler("error_msg");
+$a=10;
+echo $a/0;
 
 
---
+++ base/zifuchuang.php
@@ -0,0 +1,20 @@
+<?php
+
+
+//nowdoc
+$a=<<<'eod'
+myhcy2019dsadascascascsacdas
+asdsadasdas
+eod;
+
+
+
+$b=<<<eod
+asdasdasdascascasdasdasdasdas
+eod;
+
+echo $a."<br>";
+
+echo $b."<br>";
+
+
