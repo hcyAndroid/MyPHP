d8993d4ae1ff336af1cc247a1b28d143f68fc623
springyang <384317693@qq.com>
2019-05-31 14:06:17 +0800

    表单的注意事项

---
+++ base/表单处理.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>表单处理</title>
+</head>
+<body>
+<form action="formresult.php" method="get">
+
+    用户名 <input type="text" name="username"> <br>
+    密码   &nbsp;<input type="password" name="password"> <br>
+
+    <input type="submit" value="提交">
+    <button>提交2</button>
+
+
+</form>
+</body>
+</html>
