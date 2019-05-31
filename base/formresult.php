0cdd4a44be654d736dc56577c45c044444872790
springyang <384317693@qq.com>
2019-05-31 15:55:29 +0800

    表单处理

--- base/formresult.php
+++ base/formresult.php
@@ -1,1 +1,12 @@
 <?php
+var_dump($_GET);
+echo '<br>';
+
+var_dump($_REQUEST);
+
+
+
+//
+//foreach ($_GET as $key => $value) {
+//    echo $key.$value;
+//}
--- base/表单处理.php
+++ base/表单处理.php
@@ -1,3 +1,9 @@
+<?php
+var_dump($_POST);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +14,41 @@
 <form action="formresult.php" method="get">
 
     用户名 <input type="text" name="username"> <br>
-    密码   &nbsp;<input type="password" name="password"> <br>
-
+    密码 &nbsp;<input type="password" name="password"> <br>
     <input type="submit" value="提交">
     <button>提交2</button>
+</form>
+
+<!--直线-->
+<hr>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+
+    sex::
+    <label for=""><input type="radio" name="sex" value="1">men</label>
+    <label for=""><input type="radio" name="sex" value="2">women</label>
+    <br>
+
+    <label for=""><input type="checkbox" name="agre" value="1">agree</label>
+
+    <hr>
+
+    <label for=""><input type="checkbox" name="agre[]" value="1">agree1</label>
+    <label for=""><input type="checkbox" name="agre[]" value="2">agree2</label>
+    <label for=""><input type="checkbox" name="agre[]" value="3">agree3</label>
+
+    <button>提交</button>
+
+    <hr>
 
 
+    <select name="selectTab">
+        <option value="1" >1111</option>
+        <option value="2" >222</option>
+        <option value="3" >333</option>
+    </select>
+
+    <button>提交2</button>
 </form>
 </body>
 </html>
