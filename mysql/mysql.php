9e2ad6f43869c83747242c0c11a9ba77dd462ce7
springyang <384317693@qq.com>
2019-06-19 11:58:19 +0800

    mysql 数据库操作

---
+++ mysql/mysql.php
@@ -0,0 +1,37 @@
+<?php
+//TODO:建立数据库连接
+include_once '../utils/LogUtils.php';
+$connection = @mysqli_connect('127.0.0.1', 'root', '12345678', 'stus');
+toast($connection);
+if (!$connection) {
+    exit('mysql连接失败');
+} else {
+    echo 'mysql连接成功';
+}
+//TODO:数据库操作
+//基于数据库连接对象，执行一次数据库操作
+$query = mysqli_query($connection, 'select * from stud');
+toast($query);
+$arr = array();
+
+
+while ($row=mysqli_fetch_assoc($query)){
+    $arr[]=$row;
+}
+
+
+
+//for ($i=0;$i<$query->num_rows;$i++){
+//    $arr[] = mysqli_fetch_assoc($query);
+//}
+toast($arr);
+
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+//toast(mysqli_fetch_assoc($query));
+
