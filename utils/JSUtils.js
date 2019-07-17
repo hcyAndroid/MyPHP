356d8ce55cc1af097bd002c6bd5760070245b119
springyang <384317693@qq.com>
2019-07-17 17:49:09 +0800

    js  web api  元素节点

--- utils/JSUtils.js
+++ utils/JSUtils.js
@@ -18,3 +18,40 @@ function getInnerText(element) {
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+/**
+ * 获取第一个子元素的兼容性代码
+ * @param element
+ * @returns {ChildNode | *|Element}
+ */
+function getFirstElement(element) {
+    if (element.firstElementChild){
+        return element.firstElementChild;
+    }else {
+        var arr=element.childNodes;
+        for (let i = 0; i <arr .length; i++) {
+            if (arr[i].nodeType===1){
+                return arr[i];
+            }
+        }
+    }
+}
+
+/**
+ * 获取最后一个子元素的兼容性代码
+ * @param element
+ * @returns {ChildNode | *|Element}
+ */
+
+function getLastElement(element) {
+    if (element.lastElementChild){
+        return element.lastElementChild;
+    } else {
+        var arr=element.childNodes;
+        for (let i = arr.length-1; i >=0 ; i--) {
+            if (arr[i].nodeType===1){
+                return arr[i];
+            }
+        }
+    }
+}
