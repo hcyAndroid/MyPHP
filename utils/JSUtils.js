8c4baad10d87fe079b4fbdaca3de34b65fdef60f
springyang <384317693@qq.com>
2019-07-22 09:36:41 +0800

    js  web api  dom

--- utils/JSUtils.js
+++ utils/JSUtils.js
@@ -1,4 +1,15 @@
 /**
+ * 根据id找元素
+ * @param id
+ * @returns {HTMLElement}
+ */
+
+function my$(id) {
+    return document.getElementById(id);
+}
+
+
+/**
  * 获取上一个兄弟元素
  * @param element
  * @returns {Node | (() => (Node | null)) | ActiveX.IXMLDOMNode|null}
@@ -56,7 +67,7 @@ function setInnerText(element, value) {
     if (typeof element.innerText === 'string') {
         element.innerText = value;
     } else {
-       element.textContent =value;
+        element.textContent = value;
     }
 }
 
@@ -103,4 +114,60 @@ function getLastElement(element) {
             }
         }
     }
+}
+
+/**
+ * 注册事件的兼容性写法
+ * @param element
+ * @param eventName
+ */
+function addEventListener(element, eventName, fn) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, fn);
+    } else if (element.attachEvent) {
+        element.attachEvent('on' + eventName, fn);
+    } else {
+        element['on' + eventName] = fn;
+    }
+}
+
+/**
+ * 移除事件的兼容性写法
+ * @param element
+ * @param eventName
+ */
+
+function removeEventListener(element, eventName, fn) {
+    if (element.removeEventListener) {
+        element.removeEventListener(eventName, fn);
+    } else if (element.detachEvent) {
+        element.detachEvent('on' + eventName, fn);
+    } else {
+        element['on' + eventName] = null;
+    }
+}
+
+
+/**
+ * 获取浏览器的滑动距离
+ * @returns {{scrollLeft: number, scrollTop: number}}
+ */
+
+function getScroll() {
+    return {
+        scrollLeft: document.body.scrollLeft || document.documentElement.scrollLeft,
+        scrollTop: document.body.scrollTop || document.documentElement.scrollTop
+    };
+}
+
+/**
+ * 获取鼠标在页面位置
+ * @param e  鼠标的时间对象
+ * @returns {{pageY: (*|number), pageX: (*|number)}}
+ */
+function getMousePosition(e) {
+    return{
+        pageX:e.pageX||e.clientX+getScroll().scrollLeft,
+        pageY:e.pageY||e.clientY+getScroll().scrollTop
+    };
 }
